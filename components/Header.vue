11bfaedf1de3c0b0edc6874d353fdb2b8d373028
MishaBevz <misha.bevzenko@yandex.ru>
2022-08-31 19:26:45 +0300

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -2,6 +2,7 @@
 <template>
     <header class="header">
         <v-app-bar
+        v-if="$store.state.categoryNames.length"
         color="deep-purple accent-4"
         dense
         dark
@@ -14,6 +15,10 @@
 
             <Cart/>  
         </v-app-bar>
+
+        <v-skeleton-loader v-else
+            type="table-heading"
+        ></v-skeleton-loader>
     </header>
 </template>
 
@@ -29,6 +34,7 @@ export default {
 </script>
 <style lang="scss">
     .header {
+        height: 48px;
         margin-bottom: 42px;
     }
 </style>
